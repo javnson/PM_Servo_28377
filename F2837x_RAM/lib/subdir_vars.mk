f819ae9dd8f1df9bb90eaddf4a707a38dee505c3
javnson <65205730+javnson@users.noreply.github.com>
2021-04-30 12:53:12 +0800

    Basic

---
+++ F2837x_RAM/lib/subdir_vars.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LIB_SRCS += \
+../lib/IQmath_fpu32.lib \
+../lib/PM_bissC_Lib.lib \
+../lib/PM_endat22_lib.lib \
+../lib/Resolver_Lib_Float_TMU0.lib \
+../lib/c28x_vcu0_library_fpu32.lib 
+
+
